5feb70febc037b4ffa4014d648c4355b4547faf1
Matt Hammerly <matt.hammerly@sentry.io>
2024-09-06 14:56:49 -0700

    create test_utils crate to house all fixtures and reusable helpers

--- core/benches/pyreport.rs
+++ core/benches/pyreport.rs
@@ -5,6 +5,7 @@ use codecov_rs::{
     report::test::{TestReport, TestReportBuilder},
 };
 use divan::Bencher;
+use test_utils::fixtures::{read_fixture, FixtureFormat::Pyreport, FixtureSize::Large};
 use winnow::Parser as _;
 
 // #[global_allocator]
@@ -32,9 +33,12 @@ fn simple_report_json() {
 #[divan::bench]
 fn complex_report_json(bencher: Bencher) {
     // this is a ~11M `report_json`
-    let report = load_fixture(
-        "pyreport/large/worker-c71ddfd4cb1753c7a540e5248c2beaa079fc3341-report_json.json",
-    );
+    let report = read_fixture(
+        Pyreport,
+        Large,
+        "worker-c71ddfd4cb1753c7a540e5248c2beaa079fc3341-report_json.json",
+    )
+    .unwrap();
 
     bencher.bench(|| parse_report_json(&report));
 }
@@ -69,14 +73,21 @@ fn simple_chunks() {
 #[divan::bench(sample_count = 1)]
 fn complex_chunks(bencher: Bencher) {
     // this is a ~96M `chunks` file
-    let chunks =
-        load_fixture("pyreport/large/worker-c71ddfd4cb1753c7a540e5248c2beaa079fc3341-chunks.txt");
+    let chunks = read_fixture(
+        Pyreport,
+        Large,
+        "worker-c71ddfd4cb1753c7a540e5248c2beaa079fc3341-chunks.txt",
+    )
+    .unwrap();
     let chunks = std::str::from_utf8(&chunks).unwrap();
 
     // parsing the chunks depends on having loaded the `report_json`
-    let report = load_fixture(
-        "pyreport/large/worker-c71ddfd4cb1753c7a540e5248c2beaa079fc3341-report_json.json",
-    );
+    let report = read_fixture(
+        Pyreport,
+        Large,
+        "worker-c71ddfd4cb1753c7a540e5248c2beaa079fc3341-report_json.json",
+    )
+    .unwrap();
     let report_json::ParsedReportJson { files, sessions } = parse_report_json(&report);
 
     bencher.bench(|| parse_chunks_file(chunks, files.clone(), sessions.clone()));
@@ -94,16 +105,4 @@ fn parse_chunks_file(input: &str, files: HashMap<usize, i64>, sessions: HashMap<
     chunks::parse_chunks_file
         .parse_next(&mut chunks_stream)
         .unwrap();
-}
-
-#[track_caller]
-fn load_fixture(path: &str) -> Vec<u8> {
-    let path = format!("./fixtures/{path}");
-    let contents = std::fs::read(path).unwrap();
-
-    if contents.starts_with(b"version https://git-lfs.github.com/spec/v1") {
-        panic!("Fixture has not been pulled from Git LFS");
-    }
-
-    contents
 }
