06b847275cb9f0e7942bc8e4d38839c3dc44c5e1
Arpad Borsos <swatinem@swatinem.de>
2024-11-19 23:33:51 +0100

    Migrate benchmarks to criterion and setup `codspeed` (#60)
    This migrates the pyreport benchmarks to `criterion` via the codspeed compatibility layer.
    Additionally, this also creates a CI job to run the benchmarks within the codspeed runner, and upload the results.

--- core/benches/pyreport.rs
+++ core/benches/pyreport.rs
@@ -4,19 +4,20 @@ use codecov_rs::{
     parsers::pyreport::{chunks, report_json},
     test_utils::test_report::{TestReport, TestReportBuilder},
 };
-use divan::Bencher;
+use criterion::{criterion_group, criterion_main, Criterion};
 use test_utils::fixtures::{read_fixture, FixtureFormat::Pyreport, FixtureSize::Large};
 use winnow::Parser as _;
 
-// #[global_allocator]
-// static ALLOC: divan::AllocProfiler = divan::AllocProfiler::system();
+criterion_group!(
+    benches,
+    simple_report_json,
+    complex_report_json,
+    simple_chunks,
+    complex_chunks,
+);
+criterion_main!(benches);
 
-fn main() {
-    divan::main();
-}
-
-#[divan::bench]
-fn simple_report_json() {
+fn simple_report_json(c: &mut Criterion) {
     let reports: &[&[u8]] = &[
         br#"{"files": {"src/report.rs": [0, {}, [], null]}, "sessions": {"0": {"j": "codecov-rs CI"}}}"#,
         br#"{"files": {"src/report.rs": [0, {}, [], null], "src/report/models.rs": [1, {}, [], null]}, "sessions": {"0": {"j": "codecov-rs CI"}, "1": {"j": "codecov-rs CI 2"}}}"#,
@@ -25,13 +26,16 @@ fn simple_report_json() {
         br#"{"files": {}, "sessions": {}}"#,
     ];
 
-    for input in reports {
-        parse_report_json(input);
-    }
+    c.bench_function("simple_report_json", |b| {
+        b.iter(|| {
+            for input in reports {
+                parse_report_json(input);
+            }
+        })
+    });
 }
 
-#[divan::bench]
-fn complex_report_json(bencher: Bencher) {
+fn complex_report_json(c: &mut Criterion) {
     // this is a ~11M `report_json`
     let report = read_fixture(
         Pyreport,
@@ -40,7 +44,9 @@ fn complex_report_json(bencher: Bencher) {
     )
     .unwrap();
 
-    bencher.bench(|| parse_report_json(&report));
+    c.bench_function("complex_report_json", |b| {
+        b.iter(|| parse_report_json(&report))
+    });
 }
 
 fn parse_report_json(input: &[u8]) -> report_json::ParsedReportJson {
@@ -48,8 +54,7 @@ fn parse_report_json(input: &[u8]) -> report_json::ParsedReportJson {
     report_json::parse_report_json(input, &mut report_builder).unwrap()
 }
 
-#[divan::bench]
-fn simple_chunks() {
+fn simple_chunks(c: &mut Criterion) {
     let chunks = &[
         // Header and one chunk with an empty line
         "{}\n<<<<< end_of_header >>>>>\n{}\n",
@@ -64,14 +69,17 @@ fn simple_chunks() {
     let files = HashMap::from([(0, 0), (1, 1), (2, 2)]);
     let sessions = HashMap::from([(0, 0), (1, 1), (2, 2)]);
 
-    for input in chunks {
-        parse_chunks_file(input, files.clone(), sessions.clone())
-    }
+    c.bench_function("simple_chunks", |b| {
+        b.iter(|| {
+            for input in chunks {
+                parse_chunks_file(input, files.clone(), sessions.clone())
+            }
+        })
+    });
 }
 
 // just 1 iteration, as this is currently ~4 seconds on my machine
-#[divan::bench(sample_count = 1)]
-fn complex_chunks(bencher: Bencher) {
+fn complex_chunks(c: &mut Criterion) {
     // this is a ~96M `chunks` file
     let chunks = read_fixture(
         Pyreport,
@@ -90,7 +98,9 @@ fn complex_chunks(bencher: Bencher) {
     .unwrap();
     let report_json::ParsedReportJson { files, sessions } = parse_report_json(&report);
 
-    bencher.bench(|| parse_chunks_file(chunks, files.clone(), sessions.clone()));
+    c.bench_function("complex_chunks", |b| {
+        b.iter(|| parse_chunks_file(chunks, files.clone(), sessions.clone()))
+    });
 }
 
 fn parse_chunks_file(input: &str, files: HashMap<usize, i64>, sessions: HashMap<usize, i64>) {
